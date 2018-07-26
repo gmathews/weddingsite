3a2650faa5230865cf582542c80a4ebdadf0e6a3
George Mathews <george@georgemathe.ws>
2018-07-25 18:35:18 -0700

    Test update validation

--- server/test/rsvpTests.js
+++ server/test/rsvpTests.js
@@ -37,14 +37,15 @@ suite('RSVP', () => {
 
     const standardTestGuestName = 'test name';
     const standardTestGuestPIN = '42069';
+    const desiredMembers = {};
+    desiredMembers[standardTestGuestName] = false;
     const standardTestGuest = {
         rsvpname: standardTestGuestName,
         pin: standardTestGuestPIN,
         hasPlusOne: false,
-        members: {
-            standardTestGuestName: false
-        }
+        members: desiredMembers
     };
+
     function createStandardTestGuest(rsvp, next){
         rsvp.addItem(standardTestGuest, next);
     }
@@ -78,40 +79,26 @@ suite('RSVP', () => {
         });
     });
 
-    test('Invalid PIN Get item errors out', (done) => {
-        createStandardTestGuest(this.rsvp, (err, data)=>{
-            this.rsvp.get(standardTestGuestName, '90210', (err, guestData) =>{
-                // Should have an error
-                assert(err);
-                // Shouldn't send anything back
-                assert(!guestData);
+    function testBadGet(rsvp, name, pin, done){
+        createStandardTestGuest(rsvp, (err, data)=>{
+            rsvp.get(name, pin, (err, guestData) =>{
+                assert(err, 'Should have an error');
+                assert(!guestData, 'Shouldn\'t send anything back');
                 done();
             });
         });
+    }
+
+    test('Invalid PIN Get item errors out', (done) => {
+        testBadGet(this.rsvp, standardTestGuestName, '90210', done);
     });
 
     test('Invalid Name Get item errors out', (done) => {
-        createStandardTestGuest(this.rsvp, (err, data)=>{
-            this.rsvp.get('Invalid Name', standardTestGuestPIN, (err, guestData) =>{
-                // Should have an error
-                assert(err);
-                // Shouldn't send anything back
-                assert(!guestData);
-                done();
-            });
-        });
+        testBadGet(this.rsvp, 'Invalid Name', standardTestGuestPIN, done);
     });
 
     test('Invalid item Get item errors out', (done) => {
-        createStandardTestGuest(this.rsvp, (err, data)=>{
-            this.rsvp.get('Invalid Name', '90210', (err, guestData) =>{
-                // Should have an error
-                assert(err);
-                // Shouldn't send anything back
-                assert(!guestData);
-                done();
-            });
-        });
+        testBadGet(this.rsvp, 'Invalid Name', '90210', done);
     });
 
     //Test Update
@@ -120,10 +107,10 @@ suite('RSVP', () => {
             let params = {
                 rsvpname: standardTestGuestName,
                 pin: standardTestGuestPIN,
-                members: {
-                    standardTestGuestName: true
-                }
+                members: {}
             };
+            params.members[standardTestGuestName] = true;
+
             this.rsvp.update(params, (err, data)=>{
                 this.rsvp.get(standardTestGuestName, standardTestGuestPIN, (err, guestData)=>{
                     assert.deepEqual(guestData.members, params.members);
@@ -131,5 +118,138 @@ suite('RSVP', () => {
                 });
             });
         });
+    });
+
+    function testBadUpdate(rsvp, name, pin, done){
+        createStandardTestGuest(rsvp, (err, data)=>{
+            let params = {
+                rsvpname: name,
+                pin: pin,
+                members: {}
+            };
+            params.members[name] = true;
+
+            rsvp.update(params, (err, data)=>{
+                console.log(err, data);
+                rsvp.get(name, pin, (err, guestData)=>{
+                    console.log(err, guestData);
+                    assert(err, 'Should have an error');
+                    assert(!guestData, 'Shouldn\'t send anything back');
+                    done();
+                });
+            });
+        });
+    }
+    test('Invalid name update fails', (done) => {
+        testBadUpdate(this.rsvp, 'Invalid Name', standardTestGuestPIN, done);
+    });
+
+    test('Invalid pin update fails', (done) => {
+        testBadUpdate(this.rsvp, standardTestGuestName, '90210', done);
+    });
+
+    function testPlusOne(rsvp, going, hasPlusOne, plusOneName, done){
+        createStandardTestGuest(rsvp, (err, data)=>{
+            let params = {
+                rsvpname: standardTestGuestName,
+                pin: standardTestGuestPIN,
+                members: {}
+            };
+            params.members[standardTestGuestName] = going;
+            if(hasPlusOne){
+                params.hasPlusOne = hasPlusOne;
+            }
+            if(plusOneName){
+                params.plusOneName = plusOneName;
+            }
+
+            rsvp.update(params, (err, data)=>{
+                console.log(err, data);
+                rsvp.get(standardTestGuestName, standardTestGuestPIN, (err, guestData)=>{
+                    console.log(err, guestData);
+                    assert(!guestData.hasPlusOne,
+                        'We shouldn\'t be able to change if we are allowed a plus one');
+                    assert(!guestData.hasOwnProperty('plusOneName'),
+                        'We shouldn\' have been able to add a plus one');
+                    done();
+                });
+            });
+        });
+    }
+
+    test('Don\'t allow us to change has plus one', (done) => {
+        testPlusOne(this.rsvp, false, true, undefined, done);
+    });
+
+    test('Don\'t allow us to change has plus one if we are going', (done) => {
+        testPlusOne(this.rsvp, true, true, undefined, done);
+    });
+
+    test('Don\'t allow us to add a plus one', (done) => {
+        testPlusOne(this.rsvp, false, true, 'Wedding Crasher', done);
+    });
+
+    test('Don\'t allow us to add a plus one even if we are going', (done) => {
+        testPlusOne(this.rsvp, true, true, 'Wedding Crasher', done);
+    });
+
+    test('Don\'t allow us to add a plus one even if we don\'t modify hasPlusOne', (done) => {
+        testPlusOne(this.rsvp, false, false, 'Wedding Crasher', done);
+    });
+
+    test('Don\'t allow us to add a plus one even if we are going and we don\'t modify hasPlusOne',
+        (done) => {
+            testPlusOne(this.rsvp, true, false, 'Wedding Crasher', done);
+        });
+
+    function messWithMembers(rsvp, members, done){
+        createStandardTestGuest(rsvp, (err, data)=>{
+            let params = {
+                rsvpname: standardTestGuestName,
+                pin: standardTestGuestPIN,
+                members: members
+            };
+
+            rsvp.update(params, (err, data)=>{
+                console.log(err, data);
+                rsvp.get(standardTestGuestName, standardTestGuestPIN, (err, guestData)=>{
+                    console.log(err, guestData);
+                    assert.equal(Object.getOwnPropertyNames(guestData.members).length, 1,
+                        'Shouldn\'t be able to add items');
+                    assert(!guestData.members.hasOwnProperty('Invalid Name'),
+                        'Shouldn\'t be able to add an invalid name');
+                    done();
+                });
+            });
+        });
+    }
+    test('Don\'t allow us to change member names', (done) => {
+        let badMembers = {};
+        badMembers['Invalid Name'] = true;
+
+        messWithMembers(this.rsvp, badMembers, done);
+    });
+
+    test('Don\'t allow us to change the number of members', (done) => {
+        let badMembers = {};
+        badMembers[standardTestGuestName] = true;
+        badMembers['Invalid Name'] = true;
+
+        messWithMembers(this.rsvp, badMembers, done);
+    });
+
+    test('Don\'t allow us to change member names', (done) => {
+        let badMembers = {};
+        badMembers['Invalid Name'] = false;
+
+        messWithMembers(this.rsvp, badMembers, done);
+    });
+
+    test('Don\'t allow us to change the number of members', (done) => {
+        let badMembers = {};
+        badMembers[standardTestGuestName] = false;
+        badMembers['Invalid Name'] = false;
+
+        messWithMembers(this.rsvp, badMembers, done);
     });
 });
