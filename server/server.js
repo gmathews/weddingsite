f81b29fe4410c2118d6200220f51c2437cfb2e26
George Mathews <george@georgemathe.ws>
2018-08-06 11:14:30 -0700

    Allow www and naked domains

--- server/server.js
+++ server/server.js
@@ -41,9 +41,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 // Setup CORS for security
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://teamhangloosegetsmarried.com');
     if(process.env.ALLOW_CORS){
         res.header('Access-Control-Allow-Origin', '*');
+    }else{
+        let allowedOrigins = [ 'https://teamhangloosegetsmarried.com', 'https://www.teamhangloosegetsmarried.com'];
+        let origin = req.headers.origin;
+        if(allowedOrigins.indexOf(origin) > -1){
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
     }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
